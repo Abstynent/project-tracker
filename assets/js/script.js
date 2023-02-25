9f68d556810a77b93e176d15257c026c146ee985
Lukasz Jurkiewicz <wookash@Lukaszs-MacBook-Pro.local>
2023-02-25 21:54:11 +0000

    clock fully working now

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,12 @@
+var timeEl = $('#current-time');
+var getTime = dayjs().unix();
+timeEl.text(dayjs.unix(getTime));
+var timeInterval = setInterval(function() {
+    getTime++;
+    timeEl.text(dayjs.unix(getTime));
+}, 1000); // 1s interval
+
+
 $(function () {
     $('#datepicker').datepicker({
       changeMonth: true,
